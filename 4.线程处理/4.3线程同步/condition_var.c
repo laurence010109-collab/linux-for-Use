ef6461cf878a888edcc1b63644fb1d469da7499b
廖泽威 <zewei.liao@thundersoft.com>
2026-04-21 16:15:49 +0800

    1

---
+++ 4.线程处理/4.3线程同步/condition_var.c
@@ -0,0 +1,54 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<pthread.h>
+#include<unistd.h>
+
+#define BUFFER_SIZE 5
+int buffer[BUFFER_SIZE];
+int count=0;
+
+static pthread_mutex_t mutex=PTHREAD_MUTEX_INITIALIZER;
+static pthread_cond_t cond=PTHREAD_COND_INITIALIZER;
+
+void* producer(void* arg)
+{   int item=0;
+    while(1)
+    {   
+        
+        pthread_mutex_lock(&mutex);
+        while(count==BUFFER_SIZE)
+        {
+            pthread_cond_wait(&cond,&mutex);
+        }
+        buffer[count++]=item++;
+        printf("Produced: %d\n",buffer[count-1]);
+        pthread_cond_signal(&cond);
+        pthread_mutex_unlock(&mutex);
+    }
+}
+
+void* consumer(void* arg)
+{
+    while(1)
+    {
+        pthread_mutex_lock(&mutex);
+        while(count==0)
+        {
+            pthread_cond_wait(&cond,&mutex);
+        }
+        printf("Consumed: %d\n",buffer[--count]);
+        pthread_cond_signal(&cond);
+        pthread_mutex_unlock(&mutex);
+    }
+}
+
+int main()
+{
+    pthread_t producer_thread,consumer_thread;
+    pthread_create(&producer_thread,NULL,producer,NULL);
+    pthread_create(&consumer_thread,NULL,consumer,NULL);
+    pthread_join(producer_thread,NULL);
+    pthread_join(consumer_thread,NULL);
+    return 0;
+
+}
